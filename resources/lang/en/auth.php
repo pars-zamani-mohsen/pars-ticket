a7622e19f2128b47f1b7a7479e5b08386d4c1864
Pars zamani mohsen <pars.zamani.mohsen@gmail.com>
2024-12-31 11:17:38 +0330

    chore: add base translate file

---
+++ resources/lang/en/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'اطلاعات ورود صحیح نمی‌باشد.',
+    'password' => 'رمز عبور وارد شده صحیح نمی‌باشد.',
+    'throttle' => 'تعداد دفعات تلاش برای ورود بیش از حد مجاز است. لطفاً پس از :seconds ثانیه دوباره تلاش کنید.',
+
+];
